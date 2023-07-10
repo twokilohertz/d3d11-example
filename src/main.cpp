ce53f953f71058479bf6af4fece8404a198faae1
Adam Macdonald <72780006+twokilohertz@users.noreply.github.com>
2023-07-10 14:54:40 +0100

    clang-format added

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,32 @@
 #include <Windows.h>
+#include <cstdint>
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
 {
+    const wchar_t CLASS_NAME[] = L"D3D11 Example";
+
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+
+    if (RegisterClass(&wc) == 0)
+    {
+        return -1;
+    }
+
+    HWND hwnd =
+        CreateWindowEx(0, CLASS_NAME, L"D3D11 Example", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT,
+                       CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL);
+
+    if (hwnd == NULL)
+    {
+        return -1;
+    }
+
     return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
 }
